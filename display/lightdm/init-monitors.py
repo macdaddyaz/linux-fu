a3b16739ff9194740b60ffd493275952767dd7ff
Eric McIntyre <emcintyre@idt911.com>
2016-12-09 08:05:04 -0700

    Added monitor line parsing
    Delegated to the 'xrandr' utility, with the '--listmonitors' flag to provide concise information about the available monitors. Parsed the output into a nice, easy to use data structure.

--- display/lightdm/init-monitors.py
+++ display/lightdm/init-monitors.py
@@ -6,8 +6,10 @@ import subprocess
 ## Global constants
 ## These values might change from system to system, or from version to version
 ##
+import re
+
 LID_DISPLAY = 'eDP-1'
-PRIMARY_DISPLAYS = ['HDMI3', 'HDMI-3']
+PRIMARY_DISPLAYS = ('HDMI3', 'HDMI-3')
 LID_STATE_OPEN = 'open'
 LID_STATE_FILE = '/proc/acpi/button/lid/LID0/state'
 LID_STATE_LINE_START = 'state:'
@@ -21,9 +23,7 @@ LID_STATE_START_POS = 12
 class Lid:
     def __init__(self, lid_state):
         self.lid_state = lid_state
-
-    def is_open(self):
-        return self.lid_state == LID_STATE_OPEN
+        self.is_open = self.lid_state == LID_STATE_OPEN
 
     @classmethod
     def read_state(cls):
@@ -31,17 +31,33 @@ class Lid:
         with fileinput.input(files=(LID_STATE_FILE)) as f:
             for line in f:
                 if line.startswith(LID_STATE_LINE_START):
-                    lid_state = line[LID_STATE_START_POS:]
+                    lid_state = line[LID_STATE_START_POS:].strip()
                     break
         return Lid(lid_state)
 
-class Monitors:
-    def __init__(self):
-        pass
+class Monitor:
+    def __init__(self, monitor_line):
+        self.monitor_line = monitor_line
+        matches = re.search(r'([0-9]{1,2}): [\S]* ([\d]{3,4})/[\d]{3,4}x([\d]{3,4})/[\d]{3,4}\+([\d]{1,4})\+([\d]{1,4})[\s]*([\S]*)', self.monitor_line)
+        self.num = matches.group(1)
+        self.output = matches.group(6)
+        self.resolution = '{0}x{1}'.format(matches.group(2), matches.group(3))
+        self.position = '{0}x{1}'.format(matches.group(4), matches.group(5))
+
+    def __str__(self):
+        return self.monitor_line
+
+    def __repr__(self):
+        return self.__str__()
 
     @classmethod
     def read_state(cls):
-        pass
+        xrandr = subprocess.run(['xrandr', '--listmonitors'], universal_newlines=True, stdout=subprocess.PIPE, check=True)
+        output = xrandr.stdout
+        lines = output.splitlines()
+        return tuple(map((lambda l: Monitor(l.strip())), lines[1:]))
 
 lid = Lid.read_state()
-monitors = Monitors.read_state()
+print(lid.lid_state)
+monitors = Monitor.read_state()
+print(monitors)
